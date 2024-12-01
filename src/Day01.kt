130d85356f413d2acf1620fb6bcf9512eaa4dfd8
Artur Skowronski <me@arturskowronski.com>
2024-12-01 06:33:53 +0100

    Day 1: Historian Hysteria

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,35 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputElements = input.map() {
+            val (first, second) = it.split("   ")
+            first.toInt() to second.toInt()
+        }
+        val firstList = inputElements.map { it.first.toInt() }.sorted()
+        val secondList = inputElements.map { it.second.toInt() }.sorted()
+        return (0..(input.size - 1)).sumOf {
+            abs(firstList[it] - secondList[it])
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val inputElements = input.map() {
+            val (first, second) = it.split("   ")
+            first.toInt() to second.toInt()
+        }
+        val firstList = inputElements.map { it.first.toInt() }
+        val occurrenceList = inputElements.map { it.second.toInt() }.groupingBy { it }.eachCount()
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        return (0..(input.size - 1)).sumOf {
+            firstList[it] * occurrenceList.getOrElse(firstList[it], { 0 })
+        }
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
+    part1(testInput).println()
+    part2(testInput).println()
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
